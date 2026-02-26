8c2eec86a7cc3b2dcfea763b5ca02785c34adc6a
Jorryn13 <naidoojorryn@gmail.com>
2026-02-26 08:26:31 +0200

    First commit

---
+++ src/makinguseofescapesequence/Makinguseofescapesequence.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package makinguseofescapesequence;
+
+/**
+ *
+ * @author ST10512434
+ */
+public class Makinguseofescapesequence {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("                  --------------------                ");
+        System.out.println("   *********                             xxxxxxxxxx");
+        System.out.println("  *          *                          x           x");
+        System.out.println("*    STOP      *                       x      GO      x");
+        System.out.println("  *          *                          x            x ");
+        System.out.println("    ********                              xxxxxxxxxx");
+        System.out.println("       **                                      xx");
+        System.out.println("       **                                      xx");
+        System.out.println("       **                                      xx");
+        System.out.println("       **                                      xx");
+        System.out.println("      ****                                    xxxx");
+        System.out.println("   ***********                              xxxxxxxx");
+        System.out.println("|----------------------------------------------------------|");
+      //  System.out.println("Stop and go signs");
+        System.out.println("|----------------------------------------------------------|");
+    }
+    
+}
